908fccc755507ef367e8e1e292f4e624b07b3370
sedinkin.i <sedinkin.i@spectrumdata.ru>
2022-06-20 16:51:03 +0500

    Переход на вторую сущность(блог)

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Project(models.Model):
+    objects = None
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images')
