98358854415d0e55c060e5ca9f3de83d1d8c0a85
sedinkin.i <sedinkin.i@spectrumdata.ru>
2022-06-22 13:13:21 +0500

    Добавление перехода по ссылкам + немного визуала

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,4 @@ class Blog(models.Model):
     objects = None
     title = models.CharField(max_length=100)
     date = models.DateField(max_length=50)
-    information = models.CharField(max_length=1000)
+    information = models.TextField(max_length=1000)
